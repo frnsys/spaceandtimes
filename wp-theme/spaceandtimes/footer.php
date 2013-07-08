4bd08fbe03e76b5777e6349b657193d1dc3130b8
Francis Tseng <ftzeng@gmail.com>
2013-07-07 22:15:39 -0400

    fixes

--- wp-theme/spaceandtimes/footer.php
+++ wp-theme/spaceandtimes/footer.php
@@ -11,8 +11,7 @@
 
 <?php wp_footer(); ?>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.8.3.min.js"><\/script>')</script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.8.3.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/rainbow.js"></script>
 
